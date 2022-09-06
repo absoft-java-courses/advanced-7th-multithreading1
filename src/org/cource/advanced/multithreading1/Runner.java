a6b9a221ae85eb7ac8eb2e098bade2f60f11fd16
Dzianis Haurylavets <dzianis.haurylavets@ab-soft.net>
2022-09-06 18:35:05 +0300

    init lecture

---
+++ src/org/cource/advanced/multithreading1/Runner.java
@@ -0,0 +1,19 @@
+package org.cource.advanced.multithreading1;
+
+
+//7. Multi threading p.1:
+//    - Volatile, synchronized
+//    - Wait, notify
+//    - Locks (dead locks)
+
+
+// Plan:
+// Theory: object's monitors, synchronized, wait, notify, notifyAll
+// Simple example: 1. how-tos with basic multithreading and resource sharing. 2. Book store example (BookStore with List<Book> and sleeps)
+// Theory on volatile. No big sense for code example
+// Hard example (if we'll have a time): simultaneous push and pop tasks to the single queue
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
